9b3e9220b1eb41a7ca93abe6cd6959f1cddf6eee
Zhanna Hakhverdyan <zhakhverdyan@gmail.com>
2020-02-25 17:29:28 -0500

    exploratory data analsysis and feature selection

---
+++ src/entropy_funcs.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 21 18:12:49 2020
+
+@author: zhannahakhverdyan
+"""
+
+import math
+from collections import Counter
+from scipy.stats import entropy
+import pandas as pd
+import numpy as np
+from scipy.stats import chi2_contingency
+
+def cramers_v(x, y):
+    """A function to compute Cramer's V, effect size measure for strength of association"""
+    confusion_matrix = pd.crosstab(x,y)
+    chi2 = chi2_contingency(confusion_matrix)[0]
+    n = confusion_matrix.sum().sum()
+    phi2 = chi2/n
+    r,k = confusion_matrix.shape
+    phi2corr = max(0, phi2-((k-1)*(r-1))/(n-1))
+    rcorr = r-((r-1)**2)/(n-1)
+    kcorr = k-((k-1)**2)/(n-1)
+    return np.sqrt(phi2corr/min((kcorr-1),(rcorr-1)))
+
+def conditional_entropy(x,y):
+    """a function to quantify the amount of information needed to describe the outcome of
+    a random variable x given that the value of another random variable y is known"""
+    # entropy of x given y
+    y_counter = Counter(y)
+    xy_counter = Counter(list(zip(x,y)))
+    total_occurrences = sum(y_counter.values())
+    entropy = 0
+    for xy in xy_counter.keys():
+        p_xy = xy_counter[xy] / total_occurrences
+        p_y = y_counter[xy[1]] / total_occurrences
+        entropy += p_xy * math.log(p_y/p_xy)
+    return entropy
+
+def theils_u(x, y):
+    """a function to quantify the uncertainty coefficient or Theil's U, 
+    which is a measure of nominal association"""
+    s_xy = conditional_entropy(x,y)
+    x_counter = Counter(x)
+    total_occurrences = sum(x_counter.values())
+    p_x = list(map(lambda n: n/total_occurrences, x_counter.values()))
+    s_x = entropy(p_x)
+    if s_x == 0:
+        return 1
+    else:
+        return (s_x - s_xy) / s_x
+    
+def correlation_ratio(categories, measurements):
+    """measure of the relationship between the statistical dispersion within individual 
+    categories and the dispersion across the whole population or sample"""
+    fcat, _ = pd.factorize(categories)
+    cat_num = np.max(fcat)+1
+    y_avg_array = np.zeros(cat_num)
+    n_array = np.zeros(cat_num)
+    for i in range(0,cat_num):
+        cat_measures = measurements[np.argwhere(fcat == i).flatten()]
+        n_array[i] = len(cat_measures)
+        y_avg_array[i] = np.average(cat_measures)
+    y_total_avg = np.sum(np.multiply(y_avg_array,n_array))/np.sum(n_array)
+    numerator = np.sum(np.multiply(n_array,np.power(np.subtract(y_avg_array,y_total_avg),2)))
+    denominator = np.sum(np.power(np.subtract(measurements,y_total_avg),2))
+    if numerator == 0:
+        eta = 0.0
+    else:
+        eta = np.sqrt(numerator/denominator)
+    return eta
